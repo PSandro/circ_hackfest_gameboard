c0acfc36eecda1e9be6ce8f30eeedbf8c9628b82
Sandro Pischinger <mail@sandropischinger.de>
2023-10-15 21:52:25 +0200

    add debug display

--- tile_control/src/main.cpp
+++ tile_control/src/main.cpp
@@ -1,11 +1,15 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h>
+#include <Wire.h>
+#include <SSD1306Wire.h>
 
 #define RST_PIN D3
 #define SS_PIN D8
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
+SSD1306Wire display(0x3c, SDA, SCL);
+
 
 void init_mfrc522() {
 	mfrc522.PCD_Init();
@@ -15,27 +19,48 @@ void init_mfrc522() {
 	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
+void init_oled() {
+  display.init();
+  display.flipScreenVertically();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.display();
+
+}
+
 void setup() {
 	Serial.begin(115200);
+    init_oled();
 	SPI.begin();	
     init_mfrc522();
 }
 
 void dump_rfid_uid(byte *buffer, byte bufferSize) {
+  String s;
     for (byte i = 0; i < bufferSize; i++) {
-        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-        Serial.print(buffer[i], 16);
+      if (buffer[i] < 0x10) {
+        s += '0';
+      }
+      s += String(buffer[i], 16);
     }
+  Serial.print(s);
+  display.drawString(0, 0, s);
+  display.display();
+
 }
 
 
 int card_timeout = 0;
 
 void loop() {
+  mfrc522.PCD_Init();
+  delay(4);
   if ( !mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
     if (card_timeout > 0) {
       if (card_timeout == 1) {
         Serial.println("card removed");
+        display.clear();
+        display.display();
       }
       --card_timeout;
     }
